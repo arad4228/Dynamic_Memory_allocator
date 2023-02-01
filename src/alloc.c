0b9c4ef813572d07c45944a915af45547720e456
arad4228 <arad4228@gmail.com>
2023-02-01 20:09:47 +0900

    [KyoungJae]improve for gpt

--- src/alloc.c
+++ src/alloc.c
@@ -7,31 +7,77 @@ extern void *sbrk(intptr_t increment);
 
 unsigned int max_size;
 
+unsigned int max_size;
+
+typedef struct block {
+    struct block *next;
+    size_t size_used;
+    char data[1];
+} Block;
+
+Block *head = NULL;
+
 void *myalloc(size_t size)
 {
-    void *p = sbrk(size);
-    debug("alloc(%u): %p\n", (unsigned int)size, p);
-    max_size += size;
-    debug("max: %u\n", max_size);
-    return p;
-}
+    if (size <= 0)
+        return NULL;
 
-void *myrealloc(void *ptr, size_t size)
-{
-    void *p = NULL;
-    if (size != 0)
-    {
-        p = sbrk(size);
-        if (ptr)
-            memcpy(p, ptr, size);
-        max_size += size;
-        debug("max: %u\n", max_size);
+    Block *curr = head;
+    Block *prev = NULL;
+
+    while (curr != NULL) {
+        if ((curr->size_used & 0x01) == 0 && (curr->size_used >> 1) >= size) {
+            curr->size_used |= 0x01;
+            return curr->data;
+        }
+        prev = curr;
+        curr = curr->next;
     }
-    debug("realloc(%p, %u): %p\n", ptr, (unsigned int)size, p);
-    return p;
+
+    curr = sbrk(0);
+    void *pData = sbrk(size + sizeof(Block));
+    if (pData == (void *) -1)
+        return NULL;
+
+    curr->size_used = ((size << 1) | 0x01);
+    curr->next = NULL;
+    if (prev != NULL)
+        prev->next = curr;
+
+    max_size += size;
+    return curr->data;
 }
 
 void myfree(void *ptr)
 {
-    debug("free(%p)\n", ptr);
+    if (ptr == NULL)
+        return;
+
+    Block *curr = (Block *) (ptr - sizeof(Block));
+    curr->size_used &= ~0x01;
+}
+
+void *myrealloc(void *ptr, size_t size)
+{
+    if (ptr == NULL)
+        return myalloc(size);
+
+    if (size <= 0) {
+        myfree(ptr);
+        return NULL;
+    }
+
+    Block *curr = (Block *) (ptr - sizeof(Block));
+    if ((curr->size_used >> 1) >= size)
+        return ptr;
+
+    void *new_pData = myalloc(size);
+    if (new_pData == NULL)
+        return NULL;
+
+    size_t origin_size = curr->size_used >> 1;
+    origin_size = (origin_size < size) ? origin_size : size;
+    memcpy(new_pData, ptr, origin_size);
+    myfree(ptr);
+    return new_pData;
 }
