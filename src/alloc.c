05d30b345c2fb2d4b537da16804ce51f124caac7
arad4228 <arad4228@gmail.com>
2023-02-03 15:50:26 +0900

    [KyoungJae]Fix FixFix

--- src/alloc.c
+++ src/alloc.c
@@ -2,81 +2,74 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define POOL_SIZE 1024*1024*1024
+
 extern void debug(const char *fmt, ...);
 extern void *sbrk(intptr_t increment);
 
-typedef struct block {
-    struct block* next;
-    size_t size;
-    uint8_t used;
-    char data[1];
-} Block;
+unsigned int max_size;
+static char memory_pool[POOL_SIZE];
+static int pool_index = 0;
 
-Block* head = NULL;
+typedef struct Block Block;
+struct Block {
+    size_t size;
+    int is_used;
+    Block *next;
+};
+
+static Block *allocated_list = NULL;
+
+void add_block_to_allocated_list(Block *block) {
+    block->next = allocated_list;
+    allocated_list = block;
+}
 
 void *myalloc(size_t size)
 {
-    if (size <= 0)
+    if (pool_index + size > POOL_SIZE) {
+        // handle error, out of memory
         return NULL;
-
-    Block* curr = head;
-    Block* prev = NULL;
-
-    // Look for a block with enough space
-    while (curr != NULL)
-    {
-        if (!curr->used && curr->size >= size)
-        {
-            // Update the used field to indicate the block is now in use
-            curr->used = 1;
-            return curr->data;
-        }
-        prev = curr;
-        curr = curr->next;
     }
-
-    // Allocate a new block if no suitable block was found
-    void* pData = sbrk(size + sizeof(Block));
-
-    // Check if allocation failed
-    if (pData == (void*) -1)
-        return NULL;
-    
-    curr = (Block*)pData;
-    curr->size = size;
-    curr->used = 1;
-    curr->next = NULL;
-    if (prev != NULL)
-        prev->next = curr;
-    
-    return curr->data;
+    void *result = &memory_pool[pool_index];
+    pool_index += size;
+    return result;
 }
 
 void myfree(void *ptr)
 {
-    if (ptr == NULL)
-        return;
-    Block* curr = (Block*) (ptr - sizeof(Block));
-    curr->used = 0;
+    if(ptr == NULL) return;
+    Block *current = allocated_list;
+    Block *prev = NULL;
+    while (current != NULL) {
+        if((void *)((uint8_t*)current + sizeof(Block)) == ptr){
+            current->is_used = 0;
+            if(prev == NULL)
+                allocated_list = current->next;
+            else
+                prev->next = current->next;
+            break;
+        }
+        prev = current;
+        current = current->next;
+    }
 }
 
 void *myrealloc(void *ptr, size_t size)
 {
-    // If there is no existing space, allocate new space
-    if (ptr == NULL)
+    if (ptr == NULL) {
         return myalloc(size);
-    
-    // If size is 0, free the existing space
-    if (size <= 0)
-    {
+    }
+    if (size == 0) {
         myfree(ptr);
         return NULL;
     }
-    
-    // Otherwise, allocate new space and copy over the data
-    Block* curr = (Block*) (ptr - sizeof(Block));
-    void* new_ptr = myalloc(size);
-    memcpy(new_ptr, ptr, curr->size > size ? size : curr->size);
+    void *new_ptr = myalloc(size);
+    if (new_ptr == NULL) {
+        // handle error
+        return NULL;
+    }
+    memcpy(new_ptr, ptr, size);
     myfree(ptr);
     return new_ptr;
 }
